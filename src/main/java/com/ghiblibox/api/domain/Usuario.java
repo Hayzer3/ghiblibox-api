cb52f3422fe937e474ddf33036f5ab16d6df2488
Hayzer3 <lucashyzr@gmail.com>
2026-03-13 17:17:24 -0300

    feat: adiciona classes de seguranca e auth controller

---
+++ src/main/java/com/ghiblibox/api/domain/Usuario.java
@@ -0,0 +1,82 @@
+package com.ghiblibox.api.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "Usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    private LocalDate dataNascimento;
+
+    private String genero;
+
+    @Column(name = "qnt_filmes")
+    private Integer qntFilmes;
+
+    private Integer seguindo;
+    private Integer seguidores;
+    private Integer avaliacoes;
+
+    // METODOS OBRIGATÓRIOS DO SPRING SECURITY
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
